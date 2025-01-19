048bbf897637c92294d08548227acec21f820f00
Emanuelle <142458929+Enuly@users.noreply.github.com>
2025-01-19 19:27:15 -0300

    Delete api.crud directory

--- api.crud/config.php
+++
@@ -1,8 +0,0 @@
-<?php
- //declaração das variáveis globais
- const dbDrive = 'mysql';
- const dbHost = 'localhost';
- const dbName = 'sistema_agendamento'; 
- const dbUser = 'root';
- const dbPass ='';
-?>
